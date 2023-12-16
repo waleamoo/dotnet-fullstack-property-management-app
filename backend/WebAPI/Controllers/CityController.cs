9f7c4fc60ce7334f4abb3df4d13d8fbb52c4f6a1
Amoo Wale <amoowale@gmail.com>
2023-12-16 22:18:41 +0100

    Added the backend API

---
+++ backend/WebAPI/Controllers/CityController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Data;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        private readonly DataContext _dc;
+
+        public CityController(DataContext dc)
+        {
+            _dc = dc;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var cities = _dc.Cities.ToList();
+            return Ok(cities);
+        }
+    }
+}
---
+++ backend/WebAPI/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// 1. Add the Db context
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
